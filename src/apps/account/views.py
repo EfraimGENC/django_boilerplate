39c85f2b7ac1657eecf8dec92b425e713bdc918d
efraimgenc <efraim@kavimdigital.com>
2023-12-10 05:42:48 +0300

    flower & api view url added

--- src/apps/account/views.py
+++ src/apps/account/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
