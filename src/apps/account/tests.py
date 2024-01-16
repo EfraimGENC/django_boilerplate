41702e4d0aa2ed0f12c1306b4b25233b142e9b66
EfraimGENC <efraim@kavimdigital.com>
2024-01-16 19:33:22 +0300

    Github Actions Conf: Env added for django

--- src/apps/account/tests.py
+++ src/apps/account/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CustomUserTestCase(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create(email='efraim@kavimdigital.com', password='example*123_')
+        self.assertEqual(user.pk, user.id)
